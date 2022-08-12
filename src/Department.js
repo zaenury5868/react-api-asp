b9be6b44f1c557b0c7695e52f919fdf7347f0780
Zaenury Dhany Wibowo <zaenury5868@gmail.com>
2022-08-12 11:20:02 +0700

    bug fixing input text department and process delete

--- src/Department.js
+++ src/Department.js
@@ -24,9 +24,7 @@ export class Department extends Component{
         this.refreshList();
     }
 
-    changeDepartmentName=(e)=> {
-        this.setSate({DepartmentName:e.target.value});
-    }
+    setName = (e) => this.setState({ DepartmentName: e.target.value });
 
     addClick(){
         this.setState({
@@ -42,6 +40,62 @@ export class Department extends Component{
             DepartmentId:dep.DepartmentId,
             DepartmentName:dep.DepartmentName
         });
+    }
+
+    createClick(){
+        fetch(variables.API_URL+'department', {
+            method:'POST',
+            headers:{
+                'Accept':'application/json',
+                'Content-Type':'application/json'
+            },
+            body:JSON.stringify({
+                DepartmentName:this.state.DepartmentName
+            })
+        }).then(res=>res.json())
+            .then((result)=>{
+                alert(result);
+                this.refreshList();
+        },(error)=>{
+            alert('Failed');
+        })
+    }
+
+    updateClick(){
+        fetch(variables.API_URL+'department', {
+            method:'PUT',
+            headers:{
+                'Accept':'application/json',
+                'Content-Type':'application/json'
+            },
+            body:JSON.stringify({
+                DepartmentName:this.state.DepartmentName
+            })
+        }).then(res=>res.json())
+            .then((result)=>{
+                alert(result);
+                this.refreshList();
+        },(error)=>{
+            alert('Failed');
+        })
+    }
+
+    deleteClick(id){
+        if(window.confirm('Apa anda yakin ?')){
+            fetch(variables.API_URL+'department/'+id, {
+                method:'DELETE',
+                headers:{
+                    'Accept':'application/json',
+                    'Content-Type':'application/json'
+                }
+            }).then(res=>res.json())
+                .then((result)=>{
+                    alert(result);
+                    this.refreshList();
+            },(error)=>{
+                alert('Failed');
+            })
+        }
     }
 
     render(){
@@ -75,7 +129,7 @@ export class Department extends Component{
                                             <path fillRule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z"/>
                                         </svg>
                                     </button>    
-                                    <button type="button" className="btn btn-light mr-1">
+                                    <button type="button" className="btn btn-light mr-1" onClick={()=>this.deleteClick(dep.DepartmentId)}>
                                         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-trash3-fill" viewBox="0 0 16 16">
                                             <path d="M11 1.5v1h3.5a.5.5 0 0 1 0 1h-.538l-.853 10.66A2 2 0 0 1 11.115 16h-6.23a2 2 0 0 1-1.994-1.84L2.038 3.5H1.5a.5.5 0 0 1 0-1H5v-1A1.5 1.5 0 0 1 6.5 0h3A1.5 1.5 0 0 1 11 1.5Zm-5 0v1h4v-1a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5ZM4.5 5.029l.5 8.5a.5.5 0 1 0 .998-.06l-.5-8.5a.5.5 0 1 0-.998.06Zm6.53-.528a.5.5 0 0 0-.528.47l-.5 8.5a.5.5 0 0 0 .998.058l.5-8.5a.5.5 0 0 0-.47-.528ZM8 4.5a.5.5 0 0 0-.5.5v8.5a.5.5 0 0 0 1 0V5a.5.5 0 0 0-.5-.5Z"/>
                                         </svg>
@@ -95,10 +149,10 @@ export class Department extends Component{
                             <div className="modal-body">
                                 <div className="input-group mb-3">
                                     <span className="input-group-text">Nama Departemen</span>
-                                    <input type="text" className="form-control" value={DepartmentName} onChange={this.changeDepartmentName} />
-                                    {DepartmentId==0?<button type="button" className="btn btn-primary float-start">Create</button> : null}
-                                    {DepartmentId!=0?<button type="button" className="btn btn-primary float-start">Update</button> : null}
+                                    <input type="text" className="form-control" value={DepartmentName} onChange={this.setName} />
                                 </div>
+                                {DepartmentId==0?<button type="button" className="btn btn-primary float-start" onClick={()=>this.createClick()}>Create</button> : null}
+                                {DepartmentId!=0?<button type="button" className="btn btn-primary float-start" onClick={()=>this.updateClick()}>Update</button> : null}
                             </div>
                         </div>
                     </div>
